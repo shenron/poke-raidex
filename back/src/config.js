4a9275203041aef0dafb29e64cff9514a56cb169
Thomas CASTELLY <thomas@tcy.io>
2019-12-14 21:05:50 +0100

    back - in function of nodejs environment, change db name

--- back/src/config.js
+++ back/src/config.js
@@ -15,7 +15,7 @@ const config: {
   logFileName: 'app.log',
   dbHost: process.env.DB_HOST || 'localhost',
   dbPort: process.env.DB_PORT || '27017',
-  dbName: process.env.DB_NAME || 'dev',
+  dbName: process.env.DB_NAME || process.env.NODE_ENV === 'production' ? 'prod' : 'dev',
   serverPort: Number(process.env.BACK_PORT) || 3500,
   secure: process.env.SECURE === String(true) || process.env.NODE_ENV === 'production',
 };
